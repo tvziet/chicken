439fbfef1e00bec92af75c56f6c81548265d6289
Viet Le <vietleuit@gmail.com>
2024-05-27 06:17:16 +0700

    [C0101] Add roles table

---
+++ app/models/role.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Role < ApplicationRecord
+  # Constants to ensure we don't use invalid roles
+  ORG_ADMIN = :org_admin
+  SUPER_ADMIN = :super_admin
+  ORG_USER = :org_user
+  INDIVIDUAL_USER = :individual_user
+
+  TITLES = {
+    org_admin: 'Organization Admin',
+    super_admin: 'Super Admin',
+    org_user: 'Organization User',
+    individual_user: 'Individual User'
+  }.freeze
+
+  has_many :user_roles, dependent: :destroy
+  has_many :users, through: :user_roles
+end
