af446eaa54f052f020d0bc7c44064ee2eaaaa56c
Viet Le <vietleuit@gmail.com>
2024-05-27 06:19:30 +0700

    [C0101] Update logic sign up with roles

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,4 +1,4 @@
-# app/services/base_service.rb
+# Description: Base service class to be inherited by all services.
 class BaseService
   def self.call(*args, &block)
     new(*args, &block).call
@@ -13,6 +13,8 @@ class BaseService
   end
 
   def error_response(errors)
+    return { errors: errors.transform_values { |v| v.is_a?(Array) ? v.uniq : v } } if errors.is_a?(Hash)
+
     {
       errors: errors.each_with_object({}) do |(key, value), result|
         result[key] = value.uniq.map { |error| "#{key.to_s.capitalize} #{error}" }
