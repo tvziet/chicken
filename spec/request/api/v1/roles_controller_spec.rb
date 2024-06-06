20b480ffb18a42d9d3cba2a9d4a20153228f1b4b
Viet Le <vietleuit@gmail.com>
2024-06-06 10:48:43 +0700

    Add UT for roles in api interface

---
+++ spec/request/api/v1/roles_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Roles', type: :request do
+  let!(:org_user_role) { create(:role, name: Role::ORG_USER) }
+  let!(:individual_user_role) { create(:role, name: Role::INDIVIDUAL_USER) }
+
+  let(:headers) { { 'X-API-VERSION' => 'v1' } }
+
+  describe 'GET /roles' do
+    it 'returns a successful response' do
+      get api_roles_path, headers: headers
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns all roles order by name' do
+      get api_roles_path, headers: headers
+      expect(JSON.parse(response.body)['data'].size).to eq(2)
+      expect(JSON.parse(response.body)['data'][0]['attributes']['name']).to eq('individual_user')
+      expect(JSON.parse(response.body)['data'][1]['attributes']['name']).to eq('org_user')
+    end
+  end
+end
